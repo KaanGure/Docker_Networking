e87e9e6e5bd42a6a271b768ad7f9f1bc8df15f8b
KaanGure <kaan.gure@mail.mcgill.ca>
2022-03-18 13:11:21 -0400

    Removed f-strings

--- receive.py
+++ receive.py
@@ -3,7 +3,7 @@
 import socket
 
 ip_send = "unicorn.cim.mcgill.ca"
-#ip_send = "132.206.74.92"
+##ip_send = "132.206.74.92"
 #ip_send = "192.168.43.32"
 ip_listen = "0.0.0.0"
 receive_port = 14923
@@ -16,15 +16,15 @@ sock_send = socket.socket(socket.AF_INET,
 sock_receive = socket.socket(socket.AF_INET,
                      socket.SOCK_DGRAM)
 
-print(f'Sending {msg} to {ip_send}:{send_port}')
+print("Sending " + str(msg) + " to " + str(ip_send) + ":" + str(send_port))
 sock_send.sendto(msg, (ip_send, send_port))
 
 sock_receive.bind((ip_listen, receive_port))
-print(f'Start listening to {ip_listen}:{receive_port}')
+print("Start listening to " + str(ip_listen) + ":" + str(receive_port))
 
 while True:
     data, addr = sock_receive.recvfrom(1024) # buffer
     num = int(data)
     num = num + 1
-    print(f"received message: {data}")
-    print(f"processed message: {num}")
+    print("received message: " + str(data))
+    print("processed message: " + str(num))
--- send_receive.py
+++ send_receive.py
@@ -2,9 +2,11 @@
 
 import socket
 
-ip = "127.0.0.1"
-receive_port = 5000
-send_port = 5001
+#ip_send = "10.126.138.225"
+ip_send = "192.168.43.32"
+ip_receive = "0.0.0.0"
+receive_port = 14923
+send_port = 14923
 
 
 sock_send = socket.socket(socket.AF_INET,
@@ -13,11 +15,12 @@ sock_send = socket.socket(socket.AF_INET,
 sock_receive = socket.socket(socket.AF_INET,
                      socket.SOCK_DGRAM)
 
-sock_receive.bind((ip, receive_port))
+sock_receive.bind((ip_receive, receive_port))
 
 while True:
-    print('Start listening to ' + str(ip) + ':' + str(receive_port))
+    print('Start listening to ' + str(ip_receive) + ':' + str(receive_port))
     data, addr = sock_receive.recvfrom(1024) # buffer
+    print('recv_addr = ' + str(addr))
     #process
     num = int(data)
     num = num + 1
@@ -25,5 +28,5 @@ while True:
     print('received message: ' + str(data))
     print('processed message: ' + str(num))
     #send back
-    print('Sending ' + str(msg) + ' to ' + str(ip) + ':' + str(send_port))
-    sock_send.sendto(msg, (ip, send_port))
+    print('Sending ' + str(msg) + ' to ' + str(ip_send) + ':' + str(send_port))
+    sock_send.sendto(msg, (ip_send, send_port))
